36f545dbbedb515babebcb98d7bfc3a1f796cc3e
xzllzx <73070369+xzllzx@users.noreply.github.com>
2023-07-20 01:29:25 +0800

    todoList: Initial commit

---
+++ todo-list/src/index.js
@@ -0,0 +1,4 @@
+const todoList = (title, description, dueDate, priority, notes, checklist) => {
+  // title, description, dueDate and priority. You might also want to include notes or even a checklist
+  return { title, description, dueDate, priority, notes, checklist };
+};
