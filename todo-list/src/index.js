c8f040b544e750ffcaa1e77243a3ed22c45cf495
xzllzx <73070369+xzllzx@users.noreply.github.com>
2023-07-20 16:02:45 +0800

    Added preliminary form to create ToDo

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,7 +1,17 @@
 import "./style.css";
+import { showTodoForm, hideTodoForm } from "./form-todo.js";
 
 const headerDiv = document.querySelector("#header");
 const sidebarDiv = document.querySelector("#sidebar");
 const mainContentDiv = document.querySelector("#main-content");
 
+const createTodoButton = document.querySelector("button.create-todo");
+const createTodoForm = document.querySelector("form.create-todo");
+const submitTodoForm = document.querySelector(
+  `form.create-todo button[type='submit']`
+);
+
 console.log(headerDiv, sidebarDiv, mainContentDiv);
+
+createTodoButton.addEventListener("click", showTodoForm);
+submitTodoForm.addEventListener("click", hideTodoForm);
