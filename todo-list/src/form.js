c33de785d75a0ba4e0b0766c6fedcd2f34645300
xzllzx <73070369+xzllzx@users.noreply.github.com>
2023-07-30 12:12:33 +0800

    Added dashed red outline to invalid fields

--- todo-list/src/form.js
+++ todo-list/src/form.js
@@ -1,5 +1,9 @@
 const validateForm = (e) => {
+  const form = e.target.parentNode;
   const inputFields = Array.from(e.target.parentNode.querySelectorAll("input"));
+
+  form.classList.add("submitted");
+
   const validForm = inputFields.every((input) => input.validity.valid === true);
   return validForm;
 };
