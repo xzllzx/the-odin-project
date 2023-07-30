82a296867c49970da0dfd81772452ebc6b9c819e
xzllzx <73070369+xzllzx@users.noreply.github.com>
2023-07-30 11:21:25 +0800

    Complete form validation

--- todo-list/src/controller.js
+++ todo-list/src/controller.js
@@ -11,27 +11,29 @@ import {
   addProjectToTaskDropdown,
 } from "./dom-project-task";
 import { defaultProject, getTodayTasks, getWeekTasks } from "./initialize";
-import { validateTask } from "./form-validation";
+import { validateForm } from "./form-validation";
 
 const submitTask = (e) => {
   const taskAndProject = createTaskFromForm(e);
   const task = taskAndProject[0];
   const project = taskAndProject[1];
-  validateTask(e);
-  addTaskToProject(project, task);
-  if (project != defaultProject) addTaskToProject(defaultProject, task);
-  getTodayTasks();
-  getWeekTasks();
-  hideForms(e);
-  displayAllTasks(getCurrentPage());
+  if (validateForm(e)) {
+    addTaskToProject(project, task);
+    if (project != defaultProject) addTaskToProject(defaultProject, task);
+    getTodayTasks();
+    getWeekTasks();
+    hideForms(e);
+    displayAllTasks(getCurrentPage());
+  }
 };
 
 const submitProject = (e) => {
   const newProject = createProjectFromDom(e);
-  // validateProject(e);
-  hideForms(e);
-  addProjectToSidebar(newProject);
-  addProjectToTaskDropdown(newProject);
+  if (validateForm(e)) {
+    hideForms(e);
+    addProjectToSidebar(newProject);
+    addProjectToTaskDropdown(newProject);
+  }
 };
 
 export { submitTask, submitProject };
--- todo-list/src/form-validation.js
+++ todo-list/src/form-validation.js
@@ -1,40 +1,7 @@
-const submitButtons = document.querySelectorAll("button[type='submit']");
-
-const constraintValidation = () => {
-  for (const button of submitButtons) {
-    const inputFields = button.parentNode.querySelectorAll("input");
-
-    button.addEventListener("click", (e) => {
-      for (const input of inputFields) {
-        console.log(input.validity);
-        if (input.validity.typeMismatch) {
-          input.setCustomValidity("I am expecting an input address!");
-        } else if (input.validity.valueMissing) {
-          input.setCustomValidity("Missing input");
-        } else {
-          input.setCustomValidity("");
-        }
-      }
-    });
-  }
+const validateForm = (e) => {
+  const inputFields = Array.from(e.target.parentNode.querySelectorAll("input"));
+  const validForm = inputFields.every((input) => input.validity.valid === true);
+  return validForm;
 };
 
-const validateTask = (e) => {
-  console.log(e.target);
-  const inputFields = e.target.parentNode.querySelectorAll("input");
-
-  for (const input of inputFields) {
-    if (input.validity.typeMismatch) {
-      input.setCustomValidity("I am expecting an input address!");
-      console.log("Mismatch");
-    } else if (input.validity.valueMissing) {
-      input.setCustomValidity("Missing input");
-      console.log("Missing input");
-    } else {
-      input.setCustomValidity("");
-      console.log("Valid");
-    }
-  }
-};
-
-export { constraintValidation, validateTask };
+export { validateForm };
