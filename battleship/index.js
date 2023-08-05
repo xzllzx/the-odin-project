d579d3797e884b58b12a435f705e5e937ed7ead4
xzllzx <73070369+xzllzx@users.noreply.github.com>
2023-08-05 17:03:10 +0800

    Ship factory function

--- battleship/index.js
+++ battleship/index.js
@@ -1,3 +1,15 @@
-const Ship = () => {};
+const Ship = (length) => {
+  let timesHit = 0;
+
+  const hit = () => {
+    timesHit++;
+  };
+  const isSunk = () => {
+    return timesHit >= length;
+  };
+  return { hit, isSunk };
+};
+
 const Gameboard = () => {};
+
 const Player = () => {};
---
+++ battleship/index.test.js
@@ -0,0 +1,5 @@
+x = Ship(2);
+console.log(x.isSunk());
+x.hit();
+x.hit();
+x.isSunk();
