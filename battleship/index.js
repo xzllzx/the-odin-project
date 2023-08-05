8d5d3e7da206e1eb68291549d409cf98a1a442d9
xzllzx <73070369+xzllzx@users.noreply.github.com>
2023-08-05 17:44:19 +0800

    Game board factory function

--- battleship/index.js
+++ battleship/index.js
@@ -1,4 +1,5 @@
-const Ship = (length) => {
+const Ship = (coordinateList) => {
+  let length = coordinateList.length;
   let timesHit = 0;
 
   const hit = () => {
@@ -10,6 +11,43 @@ const Ship = (length) => {
   return { hit, isSunk };
 };
 
-const Gameboard = () => {};
+const Gameboard = (allCoordinates) => {
+  const board = [];
+  const shipList = [];
+
+  const createBoard = (height, width) => {
+    for (let i = 0; i < height; i++) {
+      const row = [];
+
+      for (let j = 0; j < width; j++) {
+        row.push(-1);
+      }
+      board.push(row);
+    }
+  };
+
+  // 4*1, 3*2, 2*3, 1*4 ships - all straight lines
+  const placeShips = (allCoordinates) => {
+    for (const [index, coordinateList] of allCoordinates.entries()) {
+      coordinateList.forEach(([row, col]) => {
+        board[row][col] = index;
+      });
+      const ship = new Ship(coordinateList);
+      shipList.push(ship);
+    }
+  };
+
+  const receiveAttack = (row, col) => {
+    if (coordinatesInShip) return true;
+  };
+
+  return { board, createBoard, placeShips, receiveAttack };
+};
 
 const Player = () => {};
+
+module.exports = {
+  Ship,
+  Gameboard,
+  Player,
+};
