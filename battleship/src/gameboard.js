788552eb47c59d4ae6bc08618b5b814a2ab1c761
xzllzx <73070369+xzllzx@users.noreply.github.com>
2023-08-05 22:27:49 +0800

    Added code to validate input ship coordinates

--- battleship/src/gameboard.js
+++ battleship/src/gameboard.js
@@ -38,15 +38,54 @@ const Gameboard = (allCoordinates) => {
     }
   };
 
+  const checkValidCoordinates = (coordinateList, height, width) => {
+    // All coordinates in list must share either a row or a column
+    // Whichever is unshared, must be consecutive
+    // Cannot be out of range
+    let sortedList;
+
+    if (coordinateList.length <= 1) return true;
+
+    // If it is both vertical and horizontal, or neither, then it is not a straight line
+    const isHorizontal = coordinateList.every(
+      (row) => row === coordinateList[0][0]
+    );
+    const isVertical = coordinateList.every(
+      (col) => col === coordinateList[0][1]
+    );
+    if (isHorizontal === isVertical)
+      throw new Error("Coordinates provided are not in a straight line");
+
+    // Check that the columns are consecutive
+    if (isHorizontal) sortedList = coordinateList.map(([row, col]) => col);
+    else if (isVertical) sortedList = coordinateList.map(([row, col]) => row);
+
+    sortedList.sort();
+
+    for (let i = 0; i < sortedList.length; i++) {
+      if (sortedList[i] !== sortedList[0] + i) {
+        throw new Error("Coordinates provided are not consecutive");
+      }
+    }
+
+    return true;
+  };
+
   // For 10*10 board: 4*1, 3*2, 2*3, 1*4 ships - all straight lines
   const placeShips = (allCoordinates) => {
     shipList = [];
     for (const [index, coordinateList] of allCoordinates.entries()) {
-      for (const [row, col] of coordinateList) {
-        board[row][col] = index;
+      try {
+        if (checkValidCoordinates(coordinateList, height, width)) {
+          for (const [row, col] of coordinateList) {
+            board[row][col] = index;
+          }
+          const newShip = Ship(coordinateList);
+          shipList.push(newShip);
+        }
+      } catch (error) {
+        throw error;
       }
-      const newShip = Ship(coordinateList);
-      shipList.push(newShip);
     }
   };
 
