1483669cad52589fb17439b82044f78097486b83
xzllzx <73070369+xzllzx@users.noreply.github.com>
2023-08-22 01:28:11 +0800

    Added some CSS for header and scoreboard

--- memory-card-game/src/components/Header.jsx
+++ memory-card-game/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 
 function Header() {
-  return <div className="header">Welcome to the Game of Memories!</div>;
+  return (
+    <div className="header">
+      <div className="welcome">Welcome to the Game of Memories!</div>
+      <div className="description">
+        Get points by clicking on an image but don't click on any more than
+        once!
+      </div>
+    </div>
+  );
 }
 
 export { Header };
