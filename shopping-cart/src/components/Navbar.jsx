b39d47dcea09d149589de46d8378021c64f13a8a
xzllzx <73070369+xzllzx@users.noreply.github.com>
2023-08-24 21:42:23 +0800

    Show number of cart items in Nav bar

--- shopping-cart/src/components/Navbar.jsx
+++ shopping-cart/src/components/Navbar.jsx
@@ -2,13 +2,23 @@ import { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import Image from "../assets/logo.png";
 
+import { CartContext } from "../routes/root";
+
 function Navbar() {
+  const cartContext = useContext(CartContext);
+
   return (
     <nav className="navigation">
       <img src={Image} alt="Logo" className="logo" />
       <Link to="/">Home</Link>
       <Link to="/shop/">Shop</Link>
-      <Link to="/cart/">Cart</Link>
+      <Link to="/cart/">
+        Cart (
+        {cartContext.cart.reduce((accumulator, currentValue) => {
+          return accumulator + currentValue;
+        }, 0)}
+        )
+      </Link>
     </nav>
   );
 }
